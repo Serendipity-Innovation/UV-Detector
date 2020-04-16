af84f6eec6edb769f28182a515f130b3e97b787b
Deerop <xchenbox@gmail.com>
2020-04-16 00:14:27 -0700

    test bench

--- UVdetection/src/Main/UVDetectionCore.java
+++ UVdetection/src/Main/UVDetectionCore.java
@@ -10,30 +10,38 @@ import api.image;
 public class UVDetectionCore {
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-	    
+		
+		//about 26 milliseconds per cycle
+		double start = System.currentTimeMillis();
+		for(int i=0; i<200; i++)process();
+		double total = System.currentTimeMillis()-start;
+		System.out.println(total/200);
+		
+		//OpenCV does bad job deleting windows
+		//So we must ourselves through catastrophic means
+		//HighGui.waitKey();
+		System.exit(69);
+	}
+	
+	
+	static void process() {
+		
 		var UV = new image("UV.jpg","With UV Light");
 		var noUV = new image("noUV.jpg","Without UV Light");
 		
-			UV.show();
-			noUV.show();
+			//UV.show();
+			//noUV.show();
 		
 		UV.findEdges();
 		noUV.findEdges();
 		
-		//image.features(UV, noUV);
-
 		UV.binarize(60);
 		noUV.binarize(120);
 		
 		UV.removeSpecks(3);
 		noUV.removeSpecks(2);
-		
-		//image.flow(UV, noUV);
-		
+				
 		UV.translate(20,25);
-		
-			//UV.show();
-			//noUV.show();	
 			
 		//UV image is ALWAYS smoother than noUV so noUV ALWAYS has more edges
 		//Therefore it is okay to subtract away edges from UV
@@ -45,12 +53,7 @@ public class UVDetectionCore {
 		UV.toColor();
 		UV.drawContours(contours);
 		
-		//noUV.show(); 
-		UV.show();
+		//UV.show();
 		
-		//OpenCV does bad job deleting windows
-		//So we must ourselves through catastrophic means
-		HighGui.waitKey();
-		System.exit(69);
 	}
 }
